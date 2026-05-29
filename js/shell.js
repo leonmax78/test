d1be2c2d8bbd89c648bb4c507eaa4235669b5d19
Leon <leonmax78@gmail.com>
2026-05-29 22:35:34 +0800

    Codex update website

--- js/shell.js
+++ js/shell.js
@@ -44,7 +44,7 @@
     </div>
     <div class="visitorCounter" aria-label="網頁瀏覽人數">
       <div class="visitorCounterTitle">網頁瀏覽人數</div>
-      <div class="visitorCounterNumber">23</div>
+      <img class="visitorCounterBadge" src="https://visitor-badge.laobi.icu/badge?page_id=leonmax78.test&left_text=%E2%80%8B&left_color=%230b84dc&right_color=%230b84dc&height=22&radius=999" alt="網頁瀏覽人數" loading="lazy">
     </div>
   </div>
 </aside>
